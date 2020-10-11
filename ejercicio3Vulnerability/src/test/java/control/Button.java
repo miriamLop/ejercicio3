6da1ccd10cf8a8de0d871318da6486ce9adb1339
miriamLop <mlopezsurco@gmail.com>
2020-10-11 01:27:07 -0400

    Ejercicio 3 vulnerabilidad

---
+++ ejercicio3Vulnerability/src/test/java/control/Button.java
@@ -0,0 +1,14 @@
+package control;
+
+import org.openqa.selenium.By;
+
+/**
+ * @autor : eynar.pari
+ * @date : 02/10/2020.
+ **/
+public class Button extends  ControlSelenium {
+
+    public Button(By locator, String myName) {
+        super(locator, myName);
+    }
+}
